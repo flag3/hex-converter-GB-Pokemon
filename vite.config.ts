caa58a798a34fc6b9482ec0085e9d5c49a29a67f
flag3 <flag3833753@gmail.com>
2023-10-14 01:15:34 +0900

    docs: ✏️ rename

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pokemon-hex-string-converter/",
+  base: "/hex-converter-GB-Pokemon/",
   plugins: [react()],
 });
