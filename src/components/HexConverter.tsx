0da58ec7ae782b9002a60aec378cfe5d0f42a88a
flag3 <flag3833753@gmail.com>
2024-11-20 01:38:50 +0900

    refactor: 💡 change the order and use Selector component

--- src/components/HexConverter.tsx
+++ src/components/HexConverter.tsx
@@ -3,6 +3,7 @@ import "./../App.css";
 import { useTranslation } from "react-i18next";
 import InputArea from "./InputArea";
 import ResetButton from "./ResetButton";
+import Selector from "./Selector";
 import {
   textToHex,
   hexToText,
@@ -11,11 +12,23 @@ import {
 } from "./../utils/hexUtils";
 
 export default function HexConverter() {
+  const { t, i18n } = useTranslation();
+  const [gen, setGen] = useState(1);
   const [text, setText] = useState("");
   const [hex, setHex] = useState("");
   const [program, setProgram] = useState("");
-  const [gen, setGen] = useState(1);
-  const { t, i18n } = useTranslation();
+
+  const handleLanguageChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    const selectedLanguage = event.target.value;
+    i18n.changeLanguage(selectedLanguage);
+  };
+
+  const handleGenChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newGen = parseInt(event.target.value);
+    setGen(newGen);
+  };
 
   const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const newText = event.target.value;
@@ -50,54 +63,33 @@ export default function HexConverter() {
     setProgram("");
   };
 
-  const handleLanguageChange = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    const selectedLanguage = event.target.value;
-    i18n.changeLanguage(selectedLanguage);
-  };
-
-  const handleGenChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const newGen = parseInt(event.target.value);
-    setGen(newGen);
-    const newHex = textToHex(text, i18n.language, newGen);
-    const newProgram = hexToProgram(newHex);
-    setHex(newHex);
-    setProgram(newProgram);
-  };
-
   return (
     <div>
       <h2>{t("title")}</h2>
-      <div>
-        {t("language")}
-        <select
-          id="language-selector"
-          onChange={handleLanguageChange}
-          value={i18n.language}
-        >
-          <option value="en">English</option>
-          <option value="fr">Français</option>
-          <option value="de">Deutsch</option>
-          <option value="it">Italiano</option>
-          <option value="es">Español</option>
-          <option value="ja">日本語</option>
-          <option value="ko">한국어</option>
-        </select>
-      </div>
+      <Selector
+        label={t("language")}
+        value={i18n.language}
+        options={[
+          { value: "en", label: "English" },
+          { value: "fr", label: "Français" },
+          { value: "de", label: "Deutsch" },
+          { value: "it", label: "Italiano" },
+          { value: "es", label: "Español" },
+          { value: "ja", label: "日本語" },
+          { value: "ko", label: "한국어" },
+        ]}
+        onChange={handleLanguageChange}
+      />
       {i18n.language !== "ko" && (
-        <div>
-          {t("gen")}
-          <select
-            id="generation-selector"
-            onChange={handleGenChange}
-            value={gen}
-            disabled={i18n.language === "ko"}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-          </select>
-        </div>
+        <Selector
+          label={t("gen")}
+          value={gen.toString()}
+          options={[
+            { value: "1", label: "1" },
+            { value: "2", label: "2" },
+          ]}
+          onChange={handleGenChange}
+        />
       )}
       <div className="input-container">
         <InputArea label={t("text")} value={text} onChange={handleTextChange} />
---
+++ src/components/Selector.tsx
@@ -0,0 +1,21 @@
+type SelectorProps = {
+  label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  options: { value: string; label: string }[];
+};
+const Selector = ({ label, value, onChange, options }: SelectorProps) => {
+  return (
+    <div>
+      {label}
+      <select value={value} onChange={onChange}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+export default Selector;
