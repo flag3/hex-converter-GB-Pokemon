11fe4521ba2f5ac286beb447a62a2a48f009272f
flag3 <flag3833753@gmail.com>
2025-08-02 20:27:00 +0900

    fix: update base path to lowercase format

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hex-converter-GB-Pokemon/",
+  base: "/hex-converter-gb-pokemon/",
   plugins: [react()],
 });
